8fcb26f3dfec6692e26fdef165664f5b419ce624
jang_jiwon <kooriangman@gmail.com>
2023-03-10 16:13:14 +0900

    [MODIFY] 컨벤션 수정

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,16 +3,20 @@ const { catchAsync } = require("../utils/error");
 
 const productsInqury = catchAsync(async (req, res) => {
   let { categoryId, limit, offset } = req.query;
+
   limit = parseInt(limit);
   offset = parseInt(offset);
+
   if (!categoryId) {
     return res.status(400).json({ message: "KEY_ERROR" });
   }
+
   const productsInqury = await productService.productsInqury(
     categoryId,
     limit,
     offset
   );
+
   return res.status(200).json({
     data: productsInqury,
   });
