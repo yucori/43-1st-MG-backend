9c8fabf014f0d14ebf8628ddc88fe399a89c02e7
jang_jiwon <kooriangman@gmail.com>
2023-03-10 16:08:18 +0900

    [MODIFY] 에러 핸들링 적용

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,26 +1,22 @@
 const productService = require("../services/productService");
+const { catchAsync } = require("../utils/error");
 
-const productsInqury = async (req, res) => {
-  try {
-    let { categoryId, limit, offset } = req.query;
-    limit = parseInt(limit);
-    offset = parseInt(offset);
-    if (!categoryId) {
-      return res.status(400).json({ message: "KEY_ERROR" });
-    }
-    const productsInqury = await productService.productsInqury(
-      categoryId,
-      limit,
-      offset
-    );
-    return res.status(200).json({
-      data: productsInqury,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(err.status || 500).json({ message: err.message });
+const productsInqury = catchAsync(async (req, res) => {
+  let { categoryId, limit, offset } = req.query;
+  limit = parseInt(limit);
+  offset = parseInt(offset);
+  if (!categoryId) {
+    return res.status(400).json({ message: "KEY_ERROR" });
   }
-};
+  const productsInqury = await productService.productsInqury(
+    categoryId,
+    limit,
+    offset
+  );
+  return res.status(200).json({
+    data: productsInqury,
+  });
+});
 
 module.exports = {
   productsInqury,
