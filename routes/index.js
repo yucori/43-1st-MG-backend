c307f7428f8bd4c487f8cc8b6932be28b77fc378
jang_jiwon <kooriangman@gmail.com>
2023-03-10 11:12:11 +0900

    [MODIFY] 충돌 해결

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
+const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
-
+router.use("/users", userRouter);
 router.use("/products", productRouter.router);
-=======
-const userRouter = require('./userRouter');
-
-router.use('/users', userRouter);
-
->>>>>>> main
 
 module.exports = router;
