2887ff64daa87ad5d7e1d8238d5ec0d72762bf50
jang_jiwon <kooriangman@gmail.com>
2023-03-14 09:54:31 +0900

    WIP

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,53 @@
-const userService = require('../services/userService')
-const { catchAsync } = require('../utils/error')
-const { emailValidator, passwordValidator } = require('../utils/validator')
+const userService = require("../services/userService");
+const { catchAsync } = require("../utils/error");
+const { emailValidator, passwordValidator } = require("../utils/validator");
 
 const signUp = catchAsync(async (req, res) => {
-  const { userName, password, email, phoneNumber, address, birth, gender, point} = req.body;
-  
-  if ( !userName || !password || !email || !phoneNumber || !address || !birth || !gender || point === undefined){
+  const {
+    userName,
+    password,
+    passwordConfirm,
+    email,
+    phoneNumber,
+    address,
+    birth,
+    gender,
+    point,
+  } = req.body;
+
+  if (
+    !userName ||
+    !password ||
+    !email ||
+    !phoneNumber ||
+    !address ||
+    !birth ||
+    !gender ||
+    point === undefined
+  ) {
     return res.status(400).json({ message: "KEY_ERROR" });
+  }
+
+  if (password != passwordConfirm) {
+    return res.status(400).json({ message: "PASSWORD_UNMATCHED" });
   }
 
   emailValidator(email);
   passwordValidator(password);
 
-  await userService.signUp(userName, password, email, phoneNumber, address, birth, gender, point);
+  await userService.signUp(
+    userName,
+    password,
+    email,
+    phoneNumber,
+    address,
+    birth,
+    gender,
+    point
+  );
   res.status(201).json({ message: "SUCCESS" });
-
-})
-
+});
 
 module.exports = {
   signUp,
-}
+};
