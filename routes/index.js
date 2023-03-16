53cdd6a3e742be26816e221c9ad5ef7f03fcdc00
jang_jiwon <kooriangman@gmail.com>
2023-03-16 19:16:05 +0900

    [MODIFY]라우터 수정

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const userRouter = require("./userRouter");
 const productRouter = require("./productRouter");
+const cartRouter = require("./cartRouter");
 const orderRouter = require("./orderRouter");
 
 router.use("/users", userRouter.router);
 router.use("/products", productRouter.router);
+router.use("/cart", cartRouter.router);
 router.use("/orders", orderRouter.router);
 
 module.exports = router;
